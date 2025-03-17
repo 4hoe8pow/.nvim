98bccb563fcd69685b678684ce827e4dc3cc7b90
4hoe8pow <tokunaga@agni.ninja>
2025-03-17 23:04:36 +0900

    🎨

---
+++ lua/plugins/akinsho_toggleterm.lua
@@ -0,0 +1,50 @@
+return {
+  "akinsho/toggleterm.nvim",
+  config = function()
+    require("toggleterm").setup{
+      -- ここに設定を追加できます
+      size = 20,
+      open_mapping = [[<c-\>]],
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      persist_size = true,
+      direction = 'float',
+      close_on_exit = true,
+      shell = vim.o.shell,
+      float_opts = {
+        border = 'curved',
+        winblend = 3,
+      }
+    }
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      dir = "git_dir",
+      direction = "float",
+      float_opts = {
+        border = "double",
+      },
+      -- function to run on opening the terminal
+      on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+      end,
+      -- function to run on closing the terminal
+      on_close = function(term)
+        vim.cmd("startinsert!")
+      end,
+    })
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+  end
+}
