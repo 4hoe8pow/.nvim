9c0fb0dc33ce8b5c5112214e4cdd7ffeb148653a
4hoe8pow <124800114+4hoe8pow@users.noreply.github.com>
2025-03-26 09:33:25 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,3 +4,4 @@ require("config.lazy")
 vim.g.mapleader = " "
 
 vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', 'jj', [[<C-\><C-n>]], { noremap = true, silent = true })
